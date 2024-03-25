8712c873e7b78b774395dc5e7a0cdef7f0343610
govor <govorinsasha0@gmail.com>
2024-03-25 10:10:39 +0100

    In commit

--- src/Book.java
+++ src/Book.java
@@ -2,14 +2,19 @@ import java.util.Objects;
 class Book {
     private String title;
     private int yearOfPublication;
+    private Author author;
 
     public Book(String title, Author author, int yearOfPublication) {
         this.title = title;
         this.yearOfPublication = yearOfPublication;
+        this.author = author;
     }
 
     public String getTitle() {
         return title;
+    }
+    public Author getAuthor() {
+        return author;
     }
     public int getYearOfPublication() {
         return yearOfPublication;
@@ -25,12 +30,13 @@ class Book {
         if (other == null || getClass() != other.getClass()) return false;
         Book book = (Book) other;
         return yearOfPublication == book.yearOfPublication &&
-                Objects.equals(title, book.title);
+                Objects.equals(title, book.title) &&
+                Objects.equals(author, book.author);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(title,yearOfPublication);
+        return Objects.hash(title, author,yearOfPublication);
     }
 
     public String toString() {
